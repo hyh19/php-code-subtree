5e64ba58c732991140f94f18fbd23b4f264e42b3
mrhuangyuhui <mrhuangyuhui@gmail.com>
2020-04-30 11:48:10 +0800

    Squashed 'lumen-framework-v5.8.13/' content from commit 5d1d1ba8d
    git-subtree-dir: lumen-framework-v5.8.13
    git-subtree-split: 5d1d1ba8dbc5b69a17370d276e96d30eb00a2b48

---
+++ tests/Http/ResponseFactoryTest.php
@@ -0,0 +1,86 @@
+<?php
+
+use Illuminate\Http\Response;
+use PHPUnit\Framework\TestCase;
+use Laravel\Lumen\Http\ResponseFactory;
+
+class ResponseFactoryTest extends TestCase
+{
+    public function testMakeDefaultResponse()
+    {
+        $content = 'hello';
+        $responseFactory = new ResponseFactory();
+        $response = $responseFactory->make($content);
+        $this->assertInstanceOf('\Symfony\Component\HttpFoundation\Response', $response);
+        $this->assertEquals($content, $response->getContent());
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+    }
+
+    public function testJsonDefaultResponse()
+    {
+        $content = ['hello' => 'world'];
+        $responseFactory = new ResponseFactory();
+        $response = $responseFactory->json($content);
+
+        $this->assertInstanceOf('\Symfony\Component\HttpFoundation\Response', $response);
+        $this->assertEquals('{"hello":"world"}', $response->getContent());
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+    }
+
+    public function testStreamDefaultResponse()
+    {
+        $responseFactory = new ResponseFactory();
+        $response = $responseFactory->stream(function () {
+            echo 'hello';
+        });
+
+        $this->assertInstanceOf('\Symfony\Component\HttpFoundation\Response', $response);
+        $this->assertFalse($response->getContent());
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+    }
+
+    public function testDownloadDefaultResponse()
+    {
+        $temp = tempnam(sys_get_temp_dir(), 'fixture');
+        $fh = fopen($temp, 'w+');
+        fwrite($fh, 'writing to tempfile');
+        fclose($fh);
+
+        $responseFactory = new ResponseFactory();
+        $response = $responseFactory->download($temp);
+
+        $this->assertInstanceOf('\Symfony\Component\HttpFoundation\Response', $response);
+        $this->assertFalse($response->getContent());
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+
+        unlink($temp);
+    }
+
+    public function testJsonResponseFromArrayableInterface()
+    {
+        // mock one Arrayable object
+        $content = $this->getMockBuilder('Illuminate\Contracts\Support\Arrayable')
+            ->setMethods(['toArray'])
+            ->getMock();
+        $content->expects($this->once())
+            ->method('toArray')
+            ->willReturn(['hello' => 'world']);
+
+        $responseFactory = new ResponseFactory();
+        $response = $responseFactory->json($content);
+
+        $this->assertInstanceOf('\Symfony\Component\HttpFoundation\Response', $response);
+        $this->assertEquals('{"hello":"world"}', $response->getContent());
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+    }
+
+    public function testStreamDeferredCallback()
+    {
+        $responseFactory = new ResponseFactory();
+        $response = $responseFactory->stream(function () {
+            $this->fail();
+        });
+
+        $this->assertFalse($response->getContent());
+    }
+}
